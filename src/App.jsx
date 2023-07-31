b0e18fe51ca0f3f50c0120b4e8fe7f41be4d9dac
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-07-31 11:36:19 +0100

    here goes nothing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css";
+const App = () => {
+  return (
+    <div className="text-2xl text-black bg-white">
+      hello
+    </div>
+  )
+}
+
+export default App
+
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
