51e7c223d726f3bfc6c98d6352d7186f8ddfbdbd
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-08-02 09:34:53 +0100

    import passwodgenerator component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./App.css";
-import LinkShortener from './components/LinkShortener';
+// import LinkShortener from './components/LinkShortener';
+import PasswordGenerator from './components/PasswordGenerator';
 const App = () => {
   return (
-    <div className="flex justify-center  h-screen bg-gray-100">
-      <div className="w-full max-w-md">
-        <h1 className="text-3xl font-bold mb-4 text-center mt-20 text-blue-500">Link Shortener</h1>
-        <LinkShortener />
+    <div className="flex justify-center  h-screen bg-[#132e4d]">
+      <div className="w-11/12 md:w-2/4">
+        {/* <h1 className="text-3xl font-bold mb-4 text-center mt-20 text-blue-500">Create strong paswords with password generator</h1> */}
+        {/* <LinkShortener /> */}
+        <PasswordGenerator />
       </div>
     </div>
   );
