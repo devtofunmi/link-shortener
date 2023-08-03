6d630699373d4a0f140a75cf575edb1f2a3969c1
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-08-03 09:14:12 +0100

    add a copy to clipboard feature

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -5,20 +5,20 @@ const PasswordGenerator = () => {
   const [length, setLength] = useState(8);
   const [includeNumbers, setIncludeNumbers] = useState(true);
   const [includeSymbols, setIncludeSymbols] = useState(true);
+  const [copied, setCopied] = useState(false);
 
   const generatePassword = () => {
-    // Define the character sets for password generation
+
     const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
     const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const numbers = "0123456789";
     const symbols = "!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?";
 
-    // Create a character set based on user choices
+
     let characterSet = lowercaseLetters + uppercaseLetters;
     if (includeNumbers) characterSet += numbers;
     if (includeSymbols) characterSet += symbols;
 
-    // Generate the password
     let generatedPassword = "";
     for (let i = 0; i < length; i++) {
       const randomIndex = Math.floor(Math.random() * characterSet.length);
@@ -40,6 +40,15 @@ const PasswordGenerator = () => {
    };
 
 
+   const copyToClipboard = () => {
+     navigator.clipboard.writeText(password).then(
+       () => setCopied(true),
+       (error) => console.error("Failed to copy password: " + error)
+     );
+     setTimeout(() => setCopied(false), 1000);
+   };
+
+
   return (
     <div className=" text-white pt-10 flex flex-col justify-center">
       <h2 className="md:text-2xl  text-xl font-bold  text-center">
@@ -48,7 +57,7 @@ const PasswordGenerator = () => {
       <p className="text-md text-center">
         Generate secure, random passwords to stay safe online.
       </p>
-      <div className="mb-4 mt-10">
+      <div className="mb-4 mt-10 font-bold">
         <label htmlFor="passwordLength" className="mr-2">
           Password Length: {length}
         </label>
@@ -63,7 +72,7 @@ const PasswordGenerator = () => {
           onChange={(e) => setLength(e.target.value)}
         />
       </div>
-      <div className="mb-4">
+      <div className="mb-4 font-semibold">
         <input
           type="checkbox"
           id="includeNumbers"
@@ -74,7 +83,7 @@ const PasswordGenerator = () => {
           Include Numbers
         </label>
       </div>
-      <div className="mb-4">
+      <div className="mb-4 font-semibold">
         <input
           type="checkbox"
           id="includeSymbols"
@@ -86,7 +95,7 @@ const PasswordGenerator = () => {
         </label>
       </div>
       <button
-        className="bg-blue-500 text-white px-4 py-2 rounded"
+        className="bg-blue-500 text-white font-bold px-4 py-2 rounded"
         onClick={generatePassword}
       >
         Generate Password
@@ -96,6 +105,15 @@ const PasswordGenerator = () => {
         <div className="border text-lg p-5 mt-2 bg-white text-black rounded-md">
           {highlightNumbers(password)}
         </div>
+        {copied && (
+          <p className="text-[#0e263b] bg-[#aafccb] mt-2 text-center w-[250px] rounded-md font-bold">Password copied to clipboard!</p>
+        )}
+        <button
+          className="mt-2 bg-blue-500 font-bold text-white px-4 py-2 rounded"
+          onClick={copyToClipboard}
+        >
+          Copy to Clipboard
+        </button>
       </div>
     </div>
   );
